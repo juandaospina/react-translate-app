66dd79179c31fe8cbca4be4c90ac6cff7e91c705
Juan Ospina <juan.ospinal@outlook.com>
2023-04-27 11:42:35 -0500

    Add new service to translate

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,13 +1,8 @@
 import axios from "axios";
 
-export const API_TRANSLATE = "https://google-translator9.p.rapidapi.com";
+export const API_TRANSLATE = "https://translate-api-one.vercel.app";
 
 export const instance = axios.create({
   baseURL: API_TRANSLATE,
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-    "X-RapidAPI-Host": "google-translator9.p.rapidapi.com",
-    "X-RapidAPI-Key": import.meta.env.VITE_TRANSLATOR_API_KEY,
-  },
   timeout: 3000,
 });
--- src/services/translator.ts
+++ src/services/translator.ts
@@ -7,22 +7,16 @@ interface Props {
   text: string;
 }
 
-export async function languageDetect(query: string) {
-  const params = `q=${query}`;
-  const res = await instance.post(`/v2/detect?${params}`);
-  const response = await res.data.data.detections[0][0].language;
-  return response;
-}
-
 export async function translator({ fromLanguage, toLanguage, text }: Props) {
-  console.log({ fromLanguage, toLanguage, text })
-  let language = fromLanguage;
-  if (fromLanguage === "auto") {
-    language = await languageDetect(text);
-  }
-  const params = `q=${text}&source=${language}&target=${toLanguage}`;
-  const res = await instance.post(`/v2?${params}`);
-  const response = await res.data.data.translations[0].translatedText;
+  const params = `q=${text}&source=${fromLanguage}&target=${toLanguage}`;
+  const response = await instance.post(`/translate?${params}`).then((res) => {
+    return res.data.translation;
+  })
+  .catch(err => {
+    console.log("[err_translate]", err)
+    throw Error(err);
+  })
+  ;
 
   return response;
 }
